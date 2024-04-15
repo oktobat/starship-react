fc6ceb5ef21991b2c7f2aa4cce180b2e3f14c2f9
oktobat <chamsogum@gmail.com>
2024-04-15 10:37:54 +0900

    4/15일 1교시

--- src/components/artist/ArtistSection.jsx
+++ src/components/artist/ArtistSection.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import styled from 'styled-components'
-import cn from 'classnames'
 
 const ArtistSectionBlock = styled.div`
     h3 {
@@ -23,6 +22,17 @@ const ArtistSectionBlock = styled.div`
             }
           }
         }
+        .artist__member {
+            display: flex;
+            flex-wrap: wrap;
+            .member__info {
+              flex: 0 0 23%;
+              margin: 20px 1%;
+              a { display: block;
+                .member__photo { img { width: 100%; } }
+              }
+            }
+        }
     }
 `
 
@@ -31,11 +41,57 @@ const ArtistSection = () => {
     const [active, setActive] = useState(0)
 
     const artistMenu = [
-        {id:0, menu: '팝 (Pop)'         },
-        {id:1, menu: '발라드 (Ballad)'  },
-        {id:2, menu: '힙합 (Hip-hop)'   },
-        {id:3, menu: '알앤비 (R&B)'     }
+        {menu: '팝 (Pop)',          id:0 },
+        {menu: '발라드 (Ballad)',   id:1 },
+        {menu: '힙합 (Hip-hop)',    id:2 },
+        {menu: '알앤비 (R&B)',      id:3 }
     ]
+
+    const artistData = {
+        0 : [ 
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '팝 크래비티', link: '#' }
+        ],
+        1 : [ 
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' },
+            { artistImg: './assets/image/p_cravity.jpg', title: '발라드 크래비티', link: '#' }  
+        ],
+        2 : [ 
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '힙합 크래비티', link: '#' }
+        ],
+        3 : [ 
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' },
+          { artistImg: './assets/image/p_cravity.jpg', title: '알앤비 크래비티', link: '#' }  
+        ]
+    }
+
 
     return (
         <ArtistSectionBlock>
@@ -43,9 +99,25 @@ const ArtistSection = () => {
             <div className="artist__wrap">
                 <div className="artist__type">
                     {
-                        artistMenu.map((item, index)=><button key={index} onClick={()=>{setActive(item.id)}} className={cn({ on:active===item.id })}>{item.menu}</button>)
+                        artistMenu.map((item, index)=><button key={index} onClick={()=>{setActive(item.id)}} className={ active===item.id && 'on' }>{item.menu}</button>)
                     }
                 </div>
+                <div className="artist__member">
+                    {
+                        artistData[active].map((item, index)=>(
+                            <div className="member__info" key={index}>
+                                <a href={item.link}>
+                                    <figure className="member__photo">
+                                        <img src={item.artistImg} alt={item.title} />
+                                    </figure>
+                                    <div className="member__desc">
+                                        <h4>{item.title}</h4>
+                                    </div>
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>    
             </div>
         </ArtistSectionBlock>
     );
